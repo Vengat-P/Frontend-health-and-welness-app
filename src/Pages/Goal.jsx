aabcd5f3f19f3996dae85c3585f2bc3f3ed67bce
Vengat-P <cronyvengat33@gmail.com>
2025-06-16 22:16:01 +0530

    updated

--- src/Pages/Goal.jsx
+++ src/Pages/Goal.jsx
@@ -1,6 +1,11 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from "react";
+import { UserContext } from "../Context/UserContext";
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+import axios from "axios";
 
 const Goal = () => {
+    const { user } = useContext(UserContext);
     return (
         <div>
             
